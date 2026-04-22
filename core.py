662cebe81802c888a2d1018e3920750d4947a488
pietro-lorenzi <4games.pilo@mail.com>
2026-04-22 20:43:15 +0200

    Avvistamento scialuppa ed epidemia completate

--- core.py
+++ core.py
@@ -84,7 +84,7 @@ merci = {
 
 # ---------- FUNZIONI CALCOLO ----------
 
-def calcola_costo_equipaggio():
+def calcola_costo_equipaggio(): #TODO sistemare
     costo_totale = 0
     for ruolo in equipaggio:
         costo_totale += equipaggio[ruolo]["numero"] * equipaggio[ruolo]["costo"]
@@ -147,10 +147,59 @@ def avvistamento_alabatro():
         stampa("Durante la navigazione avvistate un alabatro, ma sfortunatamente non avete armi a bordo per provare a colpirlo")
 
 def avvistamento_scialuppa():
-    pass
+    stampa("Durante la navigazione avvistate una scialuppa alla deriva!")
+    stampa("A bordo ci sono 4 uomini e una cassa, ma chissà cosa potrebbe mai contenere")
+    stampa("Vuoi salvare i 4 uomini?")
+    errore = True
+    while errore:
+        scelta = input(">> ").lower().strip()
+        if scelta == "si":
+            for i in range(4):
+                membro = rn.choice(list(equipaggio.keys()))
+                equipaggio[membro]["numero"] += 1
+            stampa("Decidete di salvare i 4 uomini e li accogliete a bordo, ora avete un equipaggio più numeroso!")
+            for i in merci:
+                merci[i]["numero"] += rn.randint(10,20)
+            stampa("Inoltre, all'interno della cassa trovate delle merci preziose che aumentano le vostre scorte!")
+            errore = False
+        else:
+            stampa("Decidete di non salvare i 4 uomini e proseguite la navigazione, chissà cosa c'era nella cassa...")
+            errore = False
 
 def epidemia():
-    pass
+    stampa("Durante la navigazione scoppia una terribile epidemia a bordo!")
+    medicine = merci["medicinale"]["numero"]
+    if medicine > 0:
+        stampa(f"Fortunatamente hai a disposizione {medicine} medicinali, e hai curato altrettanti membri del tuo equipaggio")
+        ammalati = []
+        for i in equipaggio:
+            ripetizioni = equipaggio[i]["numero"]
+            for x in range(ripetizioni):
+                ammalati.append(i)
+
+        for i in ammalati:
+            if rn.randint(1,10) > 7:
+                ammalati.remove(i)
+
+        for i in range(medicine):
+            if ammalati:
+                fortunato = rn.choice(ammalati)
+                ammalati.remove(fortunato)
+                medicine -= 1
+
+        for i in ammalati:
+            equipaggio[i]["numero"] -= 1
+
+        merci["medicinale"]["numero"] = medicine
+    
+    if ammalati:
+        stampa(f"Ci sono stati {len(ammalati)} morti, che corrispondono a:")
+        for i in ammalati:
+            stampa(i.upper())
+    else:
+        stampa("Incredibile! Il medico è riuscito a curare tutti!")
+    
+    stampa(f"Restano {medicine} bottiglie di medicinale")
 
 def attacco_pirata():
     pass
